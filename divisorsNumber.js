28b8edf8eb6c3ca1c8b00ce55985ff8e6f3756aa
JeyLouis <imjerrylouis@gmail.com>
2021-06-08 09:41:09 -0400

    newitem

---
+++ divisorsNumber.js
@@ -0,0 +1,15 @@
+// Count the number of divisors of a positive integer n.
+//
+// Random tests go up to n = 500000.
+
+function getDivisorsCnt(n){
+  const arr = []
+  for(let i = 1; i <= n; i++){
+    if(n % i === 0){
+     arr.push(i)
+    }
+
+  }
+  return arr.length
+  // 4 => [3]
+}
